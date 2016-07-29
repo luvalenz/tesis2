0c9e0841407261de5593f8953fba3d09846a28e4
Lucas Valenzuela <lucasvalenzuela@gmail.com>
2016-07-29 12:52:49 -0400

    asdf

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
     root_path = '/user/luvalenz'
     mac_data_path = os.path.join(root_path, 'mackenzie_data/')
     db_path = os.path.join(root_path, 'macho_training_lightcurves')
-    output_path = os.path.join(root_path, 'output')
+    output_path = '/tmp/luvalenz/output'
     n = 20000
     affinities_path = os.path.join(mac_data_path, 'twed_matrix_t_w=250_num{0}_macho.npz'.format(n))
     sample_path = os.path.join(mac_data_path, 'lcs_samples_t_w=250_num{0}_macho.pickle'.format(n))

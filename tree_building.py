43a877d9a18974cc33d8ad8a201d530633cd61ed
Lucas Valenzuela <lucasvalenzuela@gmail.com>
2016-08-26 16:23:34 -0300

    asdf

--- tree_building.py
+++ tree_building.py
@@ -78,7 +78,7 @@ def build_tree(sample_path, affinities_path, db_path,
 
 if __name__ == '__main__':
     #root_path = '/home/lucas/tesis2'
-    root_path = '/tmp/luvalenz'
+    root_path = '/mnt/nas/GrimaRepo/luvalenz'
     mac_data_path = os.path.join(root_path, 'mackenzie_data/')
     lucas_data_path = os.path.join(root_path, 'lucas_data/')
     db_path = os.path.join(root_path, 'macho_training_lightcurves')

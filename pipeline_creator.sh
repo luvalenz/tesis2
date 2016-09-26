e2c511a9e48a7032668b5aa8f8a3bb648b73e684
Lucas Valenzuela <lucasvalenzuela@gmail.com>
2016-09-26 18:11:40 -0300

    asdf

--- pipeline_creator.sh
+++ pipeline_creator.sh
@@ -8,6 +8,6 @@ do
     for n in "${ns[@]}"
     do
         screen_name=$std$ns
-        screen -S $screen_name -dm bash -c 'cd $HOME/tesis2; source venv_caleuche/bin/activate; python pipeline.py {$n} 250 10 20 {$std} weighted'
+        screen -S $screen_name -dm bash -c 'cd $HOME/tesis2; source venv_caleuche/bin/activate; python pipeline.py ${n} 250 10 20 ${std} weighted'
     done
 done

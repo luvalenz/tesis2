7ec4c36452e845c398b63525b9ca7eac2914b806
Lucas Valenzuela <lucasvalenzuela@gmail.com>
2016-09-26 15:03:36 -0300

    asdf

--- pipeline.py
+++ pipeline.py
@@ -3,10 +3,10 @@ import os
 def execute_pipleline(n_prototipes, window_size, step):
     call1 = 'python 1.sample.py lightcurves.R.txt {0} std {1} {2}'.format(n_prototipes, window_size, step)
     call2 = 'python 2.twed.py lightcurves.R.txt {0} std {1} {2}'.format(n_prototipes, window_size, step)
-    call3 = 'python 3.tree_building lightcurves.R.txt {0} std weighted {1} {2}'.format(n_prototipes, window_size, step)
+    call3 = 'python 3.tree_building.py lightcurves.R.txt {0} std weighted {1} {2}'.format(n_prototipes, window_size, step)
     model_name = 'sequence_tree_lightcurves.R.txt_{0}samples_semistdFalse_stdTrue_20levels_{1}_{2}.dill'.format(n_prototipes,
                                                                                                           window_size, step)
-    call4 = 'python 4.ndcg_test {0} 100 20'.format(model_name)
+    call4 = 'python 4.ndcg_test.py {0} 100 20'.format(model_name)
     print('RUNNING 1.SAMPLE')
     os.system(call1)
     print('RUNNING 2.TWED')

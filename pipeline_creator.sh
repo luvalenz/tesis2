6b43a3c572da2ab30f8bf9228c45194284f58725
Lucas Valenzuela <lucasvalenzuela@gmail.com>
2016-09-26 17:27:58 -0300

    asdf

--- pipeline_creator.sh
+++ pipeline_creator.sh
@@ -3,7 +3,7 @@
 #standarized, semi, and not
 
 #declare -a standarization=('std' 'semi' 'not')
-A = 'chao'
+A='chao'
 
 
 screen -S $A -dm bash -c 'cd $HOME/tesis2; source venv_caleuche/bin/activate; python pipeline.py 2 250 10 20'

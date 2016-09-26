d963681dfc9fb2fdef5990cca248f34a39f25a59
Lucas Valenzuela <lucasvalenzuela@gmail.com>
2016-09-26 17:15:32 -0300

    adsf

--- pipeline_creator.sh
+++ pipeline_creator.sh
@@ -6,4 +6,4 @@
 #a = 'hola'
 
 
-screen -dm -S hola bash -c 'cd $HOME/tesis2; source venv_caleuche/bin/activate; python pipeline.py'
+screen -dm bash -c 'cd $HOME/tesis2; source venv_caleuche/bin/activate; python pipeline.py'

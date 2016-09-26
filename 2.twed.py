cb317f8123760852312c6eaebd7f5873bfdd75f9
Lucas Valenzuela <lucasvalenzuela@gmail.com>
2016-09-26 12:44:35 -0300

    asdf

--- 2.twed.py
+++ 2.twed.py
@@ -31,9 +31,11 @@ if __name__ == '__main__':
         step = int(sys.argv[5])
     root = '/mnt/nas/GrimaRepo/luvalenz'
     input_path = 'lucas_data/subsequences_sample_{0}_n={1}_semistd{2}_std{3}_window{4}_step{5}.pickle'
-    input_path = os.path.join(root, input_path.format(lc_list_path, n_samples, semi_standardize, standardize))
+    input_path = os.path.join(root, input_path.format(lc_list_path, n_samples, semi_standardize,
+                                                      standardize, window_size, step))
     output_path = 'lucas_data/subsequences_distances_{0}_n={1}_semistd{2}_std{3}_window{4}_step{5}.npz'
-    output_path = os.path.join(root, output_path.format(lc_list_path, n_samples, semi_standardize, standardize))
+    output_path = os.path.join(root, output_path.format(lc_list_path, n_samples, semi_standardize,
+                                                        standardize, window_size, step))
     with open(input_path, 'rb') as f:
         subsequences = pickle.load(f)
     distmatrix = calculate_distances(subsequences)

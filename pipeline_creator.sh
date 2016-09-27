0e0472bbdbac134ecf0991fc68092ec4d0c65a61
Lucas Valenzuela <lucasvalenzuela@gmail.com>
2016-09-27 12:47:36 -0300

    asdf

--- pipeline_creator.sh
+++ pipeline_creator.sh
@@ -25,16 +25,30 @@
 #done
 
 #different levels
-ns=("100" "500" "1000" "5000")
+#ns=("100" "500" "1000" "5000")
 #levels=("10" "30" "40" "null")
-levels=("null")
+#
+#for n in "${ns[@]}"
+#do
+#    for level in "${levels[@]}"
+#    do
+#        screen_name="std${n}_level${level}"
+#        echo $screen_name
+#        screen -S $screen_name -dm bash -c "cd $HOME/tesis2; source venv_caleuche/bin/activate; python pipeline.py ${n} 250 10 ${level} std not"
+#    done
+#done
+
+
+#different window size
+ns=("100" "500" "1000" "5000")
+sizes=("10" "20" "50" "100" "150" "200")
 
 for n in "${ns[@]}"
 do
-    for level in "${levels[@]}"
+    for size in "${sizes[@]}"
     do
-        screen_name="std${n}_level${level}"
+        screen_name="std${n}_ws${size}"
         echo $screen_name
-        screen -S $screen_name -dm bash -c "cd $HOME/tesis2; source venv_caleuche/bin/activate; python pipeline.py ${n} 250 10 ${level} std not"
+        screen -S $screen_name -dm bash -c "cd $HOME/tesis2; source venv_caleuche/bin/activate; python pipeline.py ${n} ${size} 10 20 std not"
     done
 done

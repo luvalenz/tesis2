65abc3bf3542c4c93d4e4a449af2008a0401f6a2
Lucas Valenzuela <lucasvalenzuela@gmail.com>
2016-11-25 16:31:20 -0300

    asdf

--- 1.sample.py
+++ 1.sample.py
@@ -40,8 +40,7 @@ if input_dir != '':
 else:
     with open(input_paths_file, 'r') as f:
         lightcurves_paths = f.readlines()
-    print(lightcurves_paths[:10])
-    lightcurves_paths = [p[:-1] for p in lightcurves_paths if os.path.exists(p)]
+    lightcurves_paths = [p[:-1] for p in lightcurves_paths if os.path.exists(p[:-1])]
 
 if class_file == '':
     lightcurves_paths_sample = random.sample(lightcurves_paths, n_samples)

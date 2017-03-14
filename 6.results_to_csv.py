482bfe02881d414e79fdf5c542b966b8de1d01ed
Lucas Valenzuela <lucasvalenzuela@gmail.com>
2017-03-14 14:43:14 -0300

    asdf

--- 6.results_to_csv.py
+++ 6.results_to_csv.py
@@ -18,17 +18,23 @@ class_table_path = args.class_table_path
 results_path = args.results_path
 output_dir = args.output_dir
 
-class_table = time_series_utils.read_class_table(class_table_path)
 
+print('reading class table...')
+class_table = time_series_utils.read_class_table(class_table_path)
+print('DONE')
+
+
+print('reading class table...')
 with open(results_path, 'rb') as f:
     results = dill.load(f)
+print('DONE')
 
 target_ids = []
 target_classes = []
 ndcgs = []
 times = []
 
-
+print('Reordering results...')
 for result in results:
     target_id = result.target
     target_class, ndcg = result.ndcg(class_table)
@@ -36,6 +42,7 @@ for result in results:
     target_classes.append(target_class)
     ndcgs.append(ndcg)
     times.append(result.times)
+print('DONE')
 
 ndcg_df = pd.DataFrame(ndcgs)
 times_df = pd.DataFrame(times)
@@ -53,6 +60,8 @@ times_basename = 'times__{0}.csv'.format(results_basename)
 ndcg_output_path = os.path.join(output_dir, ndcg_basename)
 times_output_path = os.path.join(output_dir, times_basename)
 
+print('Writing files...')
 ndcg_df.to_csv(ndcg_output_path)
 times_df.to_csv(times_output_path)
+print('DONE')
 

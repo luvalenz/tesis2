262c8e9a658fb79df28e7d0b19e3d116f71b933b
Lucas Valenzuela <lucasvalenzuela@gmail.com>
2017-04-08 16:10:36 -0300

    asdf

--- subsequence_tree_4.py
+++ subsequence_tree_4.py
@@ -309,8 +309,9 @@ class Node:
     def m_vector(self):
         print('building m vector in node {}'.format(self.id))
         m = np.zeros(self.n_original_time_series_in_tree)
+        indices = {id_: index for index, id_ in enumerate(ids)}
         for key, value in self.inverted_file.items():
-            index = self.inv_[key]
+            index = indices[key]
             m[index] = value
         print('DONE')
         return m

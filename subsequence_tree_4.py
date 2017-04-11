340d8b83facb68faabfb595259ade4aef23f2db8
Lucas Valenzuela <lucasvalenzuela@gmail.com>
2017-04-10 21:08:36 -0300

    asdf

--- subsequence_tree_4.py
+++ subsequence_tree_4.py
@@ -227,19 +227,20 @@ class KMedioidsSubsequenceTree:
         d_data_frame = d_data_frame.replace([np.inf, -np.inf], np.nan).fillna(0)
         for i in d_data_frame.columns:
             print('NODE {}'.format(i))
-            old_vector = self.node_shortcuts[i].d_vector
+            old_vector = self.node_shortcuts[i].d_vector.copy()
             old_vector.sort_index()
             print('\t old d vector {}'.format(old_vector.iloc[:10]))
             print('\t old d vector shape {}'.format(self.node_shortcuts[i].d_vector.shape))
-            col = d_data_frame[i]
-            col.sort()
+            col = d_data_frame[i].copy()
+            col.sort_index()
             print('\t col {}'.format(col.iloc[:10]))
             print('\t col shape {}'.format(col.d_vector.shape))
             self.node_shortcuts[i].d_vector = col[col != 0]
-            d_vector = self.node_shortcuts[i].d_vector
-            d_vector.sort()
+            d_vector = self.node_shortcuts[i].d_vector.copy()
+            d_vector.sort_index()
             print('\t new d vector {}'.format(d_vector.iloc[:10]))
             print('\t new d vector shape {}'.format(self.node_shortcuts[i].d_vector.shape))
+            sys.exit(0)
         print('DONE')
 
 

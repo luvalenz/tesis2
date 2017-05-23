fe0a7dbdd7e9f75edb77fd3e7bf7f864afb30818
Lucas Valenzuela <lucasvalenzuela@gmail.com>
2017-05-23 17:30:21 -0400

    dasd

--- 2.2.twed.py
+++ 2.2.twed.py
@@ -6,17 +6,6 @@ from scipy.spatial.distance import squareform
 import itertools
 import pickle
 
-def chunks(l, n_chunks):
-    """Yield successive n-sized chunks from l."""
-    length = len(l)
-    n = length // n_chunks
-    for counter, i in enumerate(range(0, length, n)):
-        j = i + n
-        if counter == n_chunks - 1:
-            j = length
-        yield l[i:j]
-        if j == length:
-            break
 
 
 parser = argparse.ArgumentParser(
@@ -27,6 +16,7 @@ parser.add_argument('--dataset', required=True, type=str)
 parser.add_argument('--n_samples', required=True, type=int)
 parser.add_argument('--time_window', type=int, default=250)
 parser.add_argument('--time_step', type=int, default=10)
+parser.add_argument('--n_parts', type=int)
 
 args = parser.parse_args(sys.argv[1:])
 
@@ -36,12 +26,14 @@ dataset = args.dataset
 n_samples = args.n_samples
 time_window = args.time_window
 time_step = args.time_step
+n_parts = args.n_parts
+
 
 input_filename = 'sample_{0}_{1}_{2}_{3}.pkl'.format(dataset, n_samples,
                                                       time_window, time_step)
-input_path = os.path.join(input_dir, input_filename)
 output_filename = 'twed_{0}_{1}_{2}_{3}.pkl'.format(dataset, n_samples,
                                                       time_window, time_step)
+input_path = os.path.join(input_dir, input_filename)
 output_path = os.path.join(output_dir, output_filename)
 
 print('Loading data...')
@@ -49,18 +41,23 @@ with open(input_path, 'rb') as f:
     subsequences = pickle.load(f)
 print('DONE')
 
-print('Calculating distances...')
+
+print('getting ids...')
+ids = [s.id for s in subsequences]
+print('DONE')
+
 
 distances = []
 
-for i, j in itertools.combinations(subsequences, 2):
-    print('Calculating distanced between {} and {}'.format(i, j))
-    distances.append(time_series_twed(i, j))
+print('building distance matrix...')
+for i in range(n_parts):
+    input_path = output_path + '.part{}of{}'.format(i, n_parts)
+    with open(input_path, 'rb') as f:
+        distances += pickle.load(f)
 
-
-print('DONE')
 distance_matrix = squareform(distances)
-ids = [sub.id for sub in subsequences]
+print('DONE')
+
 
 output = {'ids': ids, 'distances': distance_matrix}
 
